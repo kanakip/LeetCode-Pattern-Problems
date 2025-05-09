183b83e1dc67e618a7030eb96981e47abfe67d89
kanakip <pallavikanaki13@gmail.com>
2025-05-09 11:04:26 +0530

    53->  Maximum Subarray

---
+++ Dynamic-programming/Maximum Subarray.java
@@ -0,0 +1,19 @@
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/description/?envType=problem-list-v2&envId=dynamic-programming
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxCurrent = nums[0];
+        int maxGlobal = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+            if (maxCurrent > maxGlobal) {
+                maxGlobal = maxCurrent;
+            }
+        }
+
+        return maxGlobal;
+    }
+}
