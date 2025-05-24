4360429dfb9721048a2e070c82b32b3031b4cff7
kanakip <pallavikanaki13@gmail.com>
2025-05-24 16:26:08 +0530

    Update 371. Sum of Two Integers.java

--- Bit Manipulation/371. Sum of Two Integers.java
+++ Bit Manipulation/371. Sum of Two Integers.java
@@ -1,8 +1,12 @@
 https://leetcode.com/problems/sum-of-two-integers/description/
 
-class Solution {
+public class Solution {
     public int getSum(int a, int b) {
-        
-        return a+b;
+        while (b != 0) {
+            int carry = a & b;  // Find carry bits
+            a = a ^ b;          // XOR adds without carrying
+            b = carry << 1;     // Shift carry left for next addition
+        }
+        return a;
     }
 }
