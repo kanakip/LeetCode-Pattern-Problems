e7d31ac9e87883fe5ea477c696441115b045e087
kanakip <pallavikanaki13@gmail.com>
2025-05-24 15:13:11 +0530

    Create 136 single-number.java

---
+++ Bit Manipulation/136 single-number.java
@@ -0,0 +1,12 @@
+
+// https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int val =0;
+        for(int i=0;i<nums.length;i++){
+            val = val^nums[i];
+        }
+        return val;
+    }
+}
